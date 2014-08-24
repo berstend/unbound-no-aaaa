5f04b838fc6d5937440a8189c2807cd3f59ebfbf
Markus <markus@haenel.pm>
2014-08-24 16:32:54 +0200

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y unbound
+sudo service unbound stop
 sudo apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y python-unbound
 #echo nameserver 127.0.0.1 | sudo tee /etc/resolv.conf
-sudo service unbound stop
